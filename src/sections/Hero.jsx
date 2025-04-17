1d60da07d6cd1872cd8936fcf567fc1552786850
Challelign Tilahun <chalie2123@gmail.com>
2025-04-17 11:22:42 +0300

    add GSAP to animate the text and add animatedCounter component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,8 +2,27 @@ import React from "react";
 import { words } from "../constants";
 import Button from "../components/Button.jsx";
 import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import AnimatedCounter from "../components/AnimatedCounter.jsx";
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero-text h1",
+      {
+        y: 50,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.2,
+        duration: 1,
+        ease: "power2.inOut",
+      }
+    );
+  });
 
   return (
     <section id="hero" className="relative overflow-hidden">
@@ -17,17 +36,17 @@ const Hero = () => {
             <div className="hero-text">
               <h1>
                 Transforming
-                  <span className="slide">
+                <span className="slide">
                   <span className="wrapper">
                     {words.map((word, index) => (
-                        <span
-                            key={index}
-                            className="flex items-center md:gap-3 gap-1 pb-2"
-                        >
+                      <span
+                        key={index}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
                         <img
-                            src={word.imgPath}
-                            alt="person"
-                            className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                          src={word.imgPath}
+                          alt="person"
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
                         />
                         <span>{word.text}</span>
                       </span>
@@ -38,22 +57,27 @@ const Hero = () => {
               <h1>into Scalable Solutions</h1>
               <h1>that Drive Real Results</h1>
             </div>
-              <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-                  👋 Hi, I’m Challelign — a software developer from Ethiopia with a strong passion for crafting clean, efficient code
-              </p>
-              <Button className="md:w-80 md:h-16 w-60 h-12" id="button" text="See my work"/>
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              👋 Hi, I’m Challelign — a software developer from Ethiopia with a
+              strong passion for crafting clean, efficient code
+            </p>
+            <Button
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="button"
+              text="See my work"
+            />
           </div>
         </header>
         {/* RIGHT 3D MODEL */}
 
-          <figure>
-              {/*<div className="hero-3d-layout border-red-200 border-2">*/}
-              <div className="hero-3d-layout  ">
-                  <HeroExperience/>
-              </div>
-          </figure>
-
+        <figure>
+          {/*<div className="hero-3d-layout border-red-200 border-2">*/}
+          <div className="hero-3d-layout  ">
+            <HeroExperience />
+          </div>
+        </figure>
       </div>
+      <AnimatedCounter />
     </section>
   );
 };
